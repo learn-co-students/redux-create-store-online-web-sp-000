30040e3248380595dc0017ebbcea4cb40d7c441c
Malcolm_Leake <echelon1089@gmail.com>
2020-08-10 20:53:02 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -14,7 +14,7 @@ function dispatch(action){
   state = reducer(state, action);
   render();
 };
-
+//
 function render() {
   let container = document.getElementById('container');
   container.textContent = state.count;
