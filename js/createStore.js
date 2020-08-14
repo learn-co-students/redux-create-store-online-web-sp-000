3df702391c902a7bed2635394717135aa1b0e22e
zdsmit <zdsmith001@gmail.com>
2020-08-14 08:23:00 -0700

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,23 @@
-let state;
+function createStore(reducer) {
+  let state;
+  // state is now accessible to dispatch
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+ 
+  return {
+    dispatch,
+    getState
+  };
+}
+
+let store = createStore(reducer)
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +29,15 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+store.dispatch({ type: '@@INIT' });
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
 })
