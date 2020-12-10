58741c79656683b9ec9e609af687d1718075d900
no0ro <nrolwes@gmail.com>
2020-12-10 02:53:37 -0500

    done

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,48 @@
-let state;
+// wrap og code in store function, pass in reducer aka just 
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+ 
+  return {
+    dispatch,
+    getState
+  };
+};
+ 
 
+// same ol reducer as before
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
+ 
     default:
       return state;
   }
-};
+}
+ 
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
+ 
+let store = createStore(reducer) // createStore takes the reducer as an argument
 
-dispatch({ type: '@@INIT' })
+
+store.dispatch({ type: '@@INIT' }); // set initial value 
+
+// Button element/listener
 let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
