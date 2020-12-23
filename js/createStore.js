e7b5a8b70ffebd2b9f2dc4d0e28d64bff0491a77
David <50710431+dmaster18@users.noreply.github.com>
2020-12-23 10:09:51 -0500

    Update createStore.js

--- js/createStore.js
+++ js/createStore.js
@@ -1,11 +1,9 @@
 let state;
 
-
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
     default:
       return state;
   }
